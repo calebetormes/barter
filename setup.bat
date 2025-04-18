5fe494cad4e1ad7c74fe1e750883aec8e46c4b51
calebetormes <calebetormes@gmail.com>
2025-04-18 18:51:10 -0300

    up

--- setup.bat
+++ setup.bat
@@ -8,12 +8,36 @@ echo ================================
 echo Instalando dependencias PHP...
 composer install
 
-:: 2.4 - Copia o arquivo .env.example para .env
+:: 2.4 - Cria o arquivo .env manualmente
 IF EXIST ".env" (
     echo Arquivo .env ja existe. Pulando esta etapa.
 ) ELSE (
-    echo Criando arquivo .env a partir do exemplo...
-    powershell -Command "Copy-Item -Path '.env.example' -Destination '.env'"
+    echo Criando arquivo temporario de configuracao .env...
+    echo APP_NAME=Laravel > temp_env.txt
+    echo APP_ENV=local >> temp_env.txt
+    echo APP_KEY= >> temp_env.txt
+    echo APP_DEBUG=true >> temp_env.txt
+    echo APP_URL=http://localhost >> temp_env.txt
+
+    echo LOG_CHANNEL=stack >> temp_env.txt
+    echo LOG_DEPRECATIONS_CHANNEL=null >> temp_env.txt
+    echo LOG_LEVEL=debug >> temp_env.txt
+
+    echo DB_CONNECTION=mysql >> temp_env.txt
+    echo DB_HOST=127.0.0.1 >> temp_env.txt
+    echo DB_PORT=3306 >> temp_env.txt
+    echo DB_DATABASE=barter >> temp_env.txt
+    echo DB_USERNAME=root >> temp_env.txt
+    echo DB_PASSWORD= >> temp_env.txt
+
+    echo SESSION_DRIVER=file >> temp_env.txt
+    echo SESSION_LIFETIME=120 >> temp_env.txt
+
+    echo VITE_APP_NAME="${APP_NAME}" >> temp_env.txt
+
+    :: Usa o comando type para garantir criação do .env
+    type temp_env.txt > ".env"
+    del temp_env.txt
 )
 
 :: 2.5 - Gera a chave da aplicação
@@ -29,6 +53,6 @@ start cmd /k "npm run dev"
 
 echo.
 echo Setup concluido!
-echo Agora configure o banco de dados no .env e rode:
+echo Agora configure o restante do .env e rode:
 echo     php artisan migrate
 pause
